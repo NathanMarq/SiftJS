c5837ec459e13b16d5deae4d132c66e5702ee868
NathanMarq <NathanMarq@gmail.com>
2014-11-12 19:27:31 -0500

    fixing the combine blender and finishing the depthEdges filter

--- src/Blenders.js
+++ src/Blenders.js
@@ -19,19 +19,18 @@ SIFT.Blender = {
   combine : function(pixelsArray){
     var count = pixelsArray.length;
     var results = SIFT.tempCTX.createImageData(pixelsArray[0].width, pixelsArray[0].height);
-    // results.putImageData(results);
 
-    for(var i=1; i<count; i++){
-      var data = pixelsArray[i];
+    for(var i=0; i<count; i++){
+      var data = pixelsArray[i].data;
 
-      for(var j=0; j<data.data.length; j+=4){
+        for(var j=0; j<data.length; j+=4){
 
-        results.data[j] += (data.data[j]*1/count);
-        results.data[j+1] += (data.data[j+1]*1/count);
-        results.data[j+2] += (data.data[j+2]*1/count);
-        results.data[j+3] += (data.data[j+3]*1/count);
+          results.data[j] += (data[j]/count);
+          results.data[j+1] += (data[j+1]/count);
+          results.data[j+2] += (data[j+2]/count);
+          results.data[j+3] += (data[j+3]/count);
 
-      }
+        }
 
     }
 
--- src/Filters.js
+++ src/Filters.js
@@ -272,8 +272,6 @@ SIFT.Filter = {
         weights[i] /= wsum;
       }
 
-      // console.log(weights);
-
       var results = SIFT.baseAlgorithms.convolveHorizontal(
                       SIFT.baseAlgorithms.convolveVertical(pixels, weights, opaque),
                       weights, opaque
@@ -285,9 +283,11 @@ SIFT.Filter = {
     },
 
     median: function(pixels, config, opaque){
-      var opaque = opaque || 1;
-      var diameter = Math.abs(config.diameter) || 1;
-      var filter;
+      opaque = opaque || 1;
+      console.log("starting median - " + config.diameter + "px");
+      var diameter = Math.abs(config.diameter) || 1,
+      iterations = config.iterations || 1,
+      filter;
 
       var radius = diameter / 2;
       var len = Math.ceil(diameter) + (1 - (Math.ceil(diameter) % 2));
@@ -308,50 +308,53 @@ SIFT.Filter = {
 
       // go through the destination image pixels
       var alphaFac = opaque ? 1 : 0;
-      for (var y=0; y<h; y++) {
-        for (var x=0; x<w; x++) {
-          var sourceY = y;
-          var sourceX = x;
-          var dstOff = (y*w+x)*4;
+      // for(var i=0; i<iterations; i++){
+      //   console.log("iteration number " + i);
+        for (var y=0; y<h; y++) {
+          for (var x=0; x<w; x++) {
+            var sourceY = y;
+            var sourceX = x;
+            var dstOff = (y*w+x)*4;
 
-          // calculate the median values from the list of rgb values that
-          // fall under the convolution matrix
-          var r=[], g=[], b=[], a=[];
-          for (var cy=0; cy<side; cy++) {
-            for (var cx=0; cx<side; cx++) {
+            // calculate the median values from the list of rgb values that
+            // fall under the convolution matrix
+            var r=[], g=[], b=[], a=[];
+            for (var cy=0; cy<side; cy++) {
+              for (var cx=0; cx<side; cx++) {
 
-              var sourceConvY = sourceY + cy - halfSide;
-              var sourceConvX = sourceX + cx - halfSide;
+                var sourceConvY = sourceY + cy - halfSide;
+                var sourceConvX = sourceX + cx - halfSide;
 
-              // if the convolution kernel goes off the edge of the image,
-              // take the next pixel from the other side of the source image's edge
-              // (works best with repeated textures, perhaps have an option here?)
-              sourceConvY = (sourceConvY > sh || sourceConvY < 0) ? (Math.abs(sourceConvY % sh)) : sourceConvY;
-              sourceConvX = (sourceConvX > sw || sourceConvX < 0) ? (Math.abs(sourceConvX % sw)) : sourceConvX;
+                // if the convolution kernel goes off the edge of the image,
+                // take the next pixel from the other side of the source image's edge
+                // (works best with repeated textures, perhaps have an option here?)
+                sourceConvY = (sourceConvY > sh || sourceConvY < 0) ? (Math.abs(sourceConvY % sh)) : sourceConvY;
+                sourceConvX = (sourceConvX > sw || sourceConvX < 0) ? (Math.abs(sourceConvX % sw)) : sourceConvX;
 
-              var srcOff = (sourceConvY*sw+sourceConvX)*4;
-              // var wt = kernel[cy*side+cx];
+                var srcOff = (sourceConvY*sw+sourceConvX)*4;
+                // var wt = kernel[cy*side+cx];
 
-              r.push(src[srcOff]);
-              g.push(src[srcOff+1]);
-              b.push(src[srcOff+2]);
-              a.push(src[srcOff+3]);
+                r.push(src[srcOff]);
+                g.push(src[srcOff+1]);
+                b.push(src[srcOff+2]);
+                a.push(src[srcOff+3]);
 
+              }
             }
+
+            r = r.sort(function(a, b) { return a - b; });
+            g = g.sort(function(a, b) { return a - b; });
+            b = b.sort(function(a, b) { return a - b; });
+            a = a.sort(function(a, b) { return a - b; });
+
+            dst[dstOff] = r[middle];
+            dst[dstOff+1] = g[middle];
+            dst[dstOff+2] = b[middle];
+            dst[dstOff+3] = a[middle];
           }
-
-          r = r.sort(function(a, b) { return a - b; });
-          g = g.sort(function(a, b) { return a - b; });
-          b = b.sort(function(a, b) { return a - b; });
-          a = a.sort(function(a, b) { return a - b; });
-
-          dst[dstOff] = r[middle];
-          dst[dstOff+1] = g[middle];
-          dst[dstOff+2] = b[middle];
-          dst[dstOff+3] = a[middle];
         }
-      }
-
+        console.log("finishing median");
+      // }
       return output;
 
     },
@@ -389,157 +392,58 @@ SIFT.Filter = {
 
     },
 
-    //@TODO:
-    // We want to generate, in total:
-    // o Normal Map
-    // o Height Map (based off of normal map?)
-    // o Specularity Map
-    // o Ambient Occlusion Map (multiplier for diffuse map?)
-
-    // combination of sobel and others?
-    // http://hlevkin.com/articles/SobelScharrGradients5x5.pdf
-
-    // shape from shading:
-    // http://www.sci.utah.edu/~gerig/CS6320-S2013/Materials/Elhabian_SFS08.pdf
-
-    // appgen material generator
-    // http://www.dongallen.com/project/appgen/appgen_paper.pdf
-
-    // Laplacian for Blob detection?
-    // http://progmat.uw.hu/oktseg/kepelemzes/lec04_highpass_4.pdf
-
-    //@TODO
-    // Cache the original kirsch operations, and apply the other filters after the fact,
-    // so we're not running the kirsch operations every time settings change. Maybe
-    // have some manager that handles various operations seperately, then combines
-    // the resulting canvas data?
-
-    // Median filter for large details
-    // bilateral filter for medium ones (possibly thresholding the original instead?)
-
-    // REMEMBER: FIX to use the strength value to push the rgb values closer
-    // or further from 128,128,255, the neutral (rather than darkening or brightening)
-
-    // keep an eye on where we do canvas sizing, we only want to size canvases via css
-
-    depthChannels: function(pixels, config){
-      var smallStrength = config.smallStrength || 0.5; // 0 to 1 value
-      var mediumStrength = config.mediumStrength || 0.5; // 0 to 1 value
-      var largeStrength = config.largeStrength || 0.5; // 0 to 1 value
-      var invertX = (config.invertX) ? 1 : -1;
-      var invertY = (config.invertY) ? 1 : -1;
-      var strengthCount = config.count || 3;
-
-      var smallStrengthX = (smallStrength*invertX);
-      var smallStrengthY = (smallStrength*invertY);
-
-      // large details are based off the small detail filter, so we don't
-      // need to have the invert multiplier
-      var largeStrengthX = (largeStrength);
-      var largeStrengthY = (largeStrength);
-
-      // var grayscale = SIFT.Filter.detectShadows(pixels, 1);
-      var grayscale = SIFT.Filter.blackAndWhite(pixels, { level: 1 });
-
-      // for (var i=0; i<pixels.data.length; i+=4) {
-      //
-      //   var factor = (grayscale.data[i]*0.8)+(shadows.data[i]);
-      //
-      //   grayscale.data[i] = factor;
-      //   grayscale.data[i+1] = factor;
-      //   grayscale.data[i+2] = factor;
-      //
-      // }
-
-
-      // We need to use Float32 values here, because the outputs range from negative numbers to positive,
-      // and we want to use them for mapping to the 0-255 range.
-      var north = SIFT.baseAlgorithms.convolveKernelFloat32(grayscale, SIFT.Kernel.kirsch.north);
-      var northEast = SIFT.baseAlgorithms.convolveKernelFloat32(grayscale, SIFT.Kernel.kirsch.northEast);
-      var east = SIFT.baseAlgorithms.convolveKernelFloat32(grayscale, SIFT.Kernel.kirsch.east);
-      var southEast = SIFT.baseAlgorithms.convolveKernelFloat32(grayscale, SIFT.Kernel.kirsch.southEast);
-      var south = SIFT.baseAlgorithms.convolveKernelFloat32(grayscale, SIFT.Kernel.kirsch.south);
-      var southWest = SIFT.baseAlgorithms.convolveKernelFloat32(grayscale, SIFT.Kernel.kirsch.southWest);
-      var west = SIFT.baseAlgorithms.convolveKernelFloat32(grayscale, SIFT.Kernel.kirsch.west);
-      var northWest = SIFT.baseAlgorithms.convolveKernelFloat32(grayscale, SIFT.Kernel.kirsch.northWest);
-
-      var imageEdges = SIFT.tempCTX.createImageData(grayscale.width, grayscale.height);
-
-      for (var i=0; i<imageEdges.data.length; i+=4) {
-
-        // make the vertical gradient red
-        var vWeights = west.data[i] + ( ( northWest.data[i] + southWest.data[i] )/2)  - (( northEast.data[i] + southEast.data[i] )/2);
-        var vertical = ( ( vWeights * (smallStrengthY) ) + 1020 ) * ( 255 / 2040 );
-        // var horizontal = ( ( southEast.data[i] * smallStrengthY ) + 1020 ) * ( 255 / 2040 );
-        imageEdges.data[i] = vertical;
-
-        // make the horizontal gradient green
-        var hWeights = north.data[i] + ( ( northEast.data[i] + northWest.data[i] )/2 ) - ( ( southEast.data[i] + southWest.data[i] )/2 );
-        var horizontal = ( ( hWeights * (smallStrengthX) ) + 1020 ) * ( 255 / 2040 );
-        // var horizontal = ( ( northWest.data[i] * smallStrengthX ) + 1020 ) * ( 255 / 2040 );
-
-        imageEdges.data[i+1] = horizontal;
-
-        // we want to lower the blue when there are more of the other colors
-        imageEdges.data[i+2] = 255;
-
-        imageEdges.data[i+3] = pixels.data[i+3]; // keep whatever the alpha channel is
-      }
-
-      var mediumDetails = SIFT.Filter.gaussianBlur(imageEdges, {diameter: Math.ceil(pixels.width/25)}, 0);
-      var largeDetails = SIFT.Filter.gaussianBlur(imageEdges, {diameter: Math.ceil(pixels.width/40)}, 0);
-
-      for(var j=0; j<imageEdges.data.length; j+=4){
-        imageEdges.data[j] = (imageEdges.data[j] + (largeDetails.data[j]*largeStrength) + (mediumDetails.data[j]*mediumStrength))/strengthCount;
-        imageEdges.data[j+1] = (imageEdges.data[j+1] + (largeDetails.data[j]*largeStrength) + (mediumDetails.data[j+1]*mediumStrength))/strengthCount;
-        imageEdges.data[j+2] = (imageEdges.data[j+2] + (largeDetails.data[j]*largeStrength) + (mediumDetails.data[j+2]*mediumStrength))/strengthCount;
-      }
-
-      return imageEdges;
-
-    },
-
     depthEdges: function(pixels){
-
+      console.log('starting kirsch');
       var grayscale = SIFT.Filter.blackAndWhite(pixels, { level: 1 });
 
       // We need to use Float32 values here, because the outputs range from negative numbers to positive,
       // and we want to use them for mapping to the 0-255 range.
       var north = SIFT.baseAlgorithms.convolveKernelFloat32(grayscale, SIFT.Kernel.kirsch.north);
+        console.log('kirsch North');
       var northEast = SIFT.baseAlgorithms.convolveKernelFloat32(grayscale, SIFT.Kernel.kirsch.northEast);
+        console.log('kirsch NorthEast');
       var east = SIFT.baseAlgorithms.convolveKernelFloat32(grayscale, SIFT.Kernel.kirsch.east);
+        console.log('kirsch East');
       var southEast = SIFT.baseAlgorithms.convolveKernelFloat32(grayscale, SIFT.Kernel.kirsch.southEast);
+        console.log('kirsch SouthEast');
       var south = SIFT.baseAlgorithms.convolveKernelFloat32(grayscale, SIFT.Kernel.kirsch.south);
+        console.log('kirsch South');
       var southWest = SIFT.baseAlgorithms.convolveKernelFloat32(grayscale, SIFT.Kernel.kirsch.southWest);
+        console.log('kirsch SouthWest');
       var west = SIFT.baseAlgorithms.convolveKernelFloat32(grayscale, SIFT.Kernel.kirsch.west);
+        console.log('kirsch West');
       var northWest = SIFT.baseAlgorithms.convolveKernelFloat32(grayscale, SIFT.Kernel.kirsch.northWest);
+        console.log('kirsch NorthWest');
 
       var imageEdges = SIFT.tempCTX.createImageData(grayscale.width, grayscale.height);
 
       for (var i=0; i<imageEdges.data.length; i+=4) {
 
         // make the vertical gradient red
-        var vWeights = west.data[i] + ( ( northWest.data[i] + southWest.data[i] )/2)  - (( northEast.data[i] + southEast.data[i] )/2);
+        var vWeights = west.data[i] + ( ( northWest.data[i] + southWest.data[i] ) )  - ( ( northEast.data[i] + southEast.data[i] ) );
+        // var vWeights = west.data[i] + ( ( northWest.data[i] + southWest.data[i] ) );
         var vertical = ( vWeights + 1020 ) * ( 255 / 2040 );
 
         imageEdges.data[i] = vertical;
 
 
         // make the horizontal gradient green
-        var hWeights = north.data[i] + ( ( northEast.data[i] + northWest.data[i] )/2 ) - ( ( southEast.data[i] + southWest.data[i] )/2 );
+        var hWeights = north.data[i] + ( ( northEast.data[i] + northWest.data[i] ) ) - ( ( southEast.data[i] + southWest.data[i] ) );
+        // var hWeights = north.data[i] + ( ( northEast.data[i] + northWest.data[i] ) );
         var horizontal = ( hWeights + 1020 ) * ( 255 / 2040 );
 
         imageEdges.data[i+1] = horizontal;
 
-
+        var direction = (vWeights > hWeights) ? vertical : horizontal;
 
         // make blue universally 100%
-        imageEdges.data[i+2] = 255;
+        imageEdges.data[i+2] = 255 - Math.round(direction/2);
 
 
 
         imageEdges.data[i+3] = pixels.data[i+3]; // keep whatever the alpha channel is
       }
+      console.log('finishing kirsch');
 
       return imageEdges;
 
@@ -561,8 +465,8 @@ SIFT.Filter = {
 
         pixels.data[i] = neutralR + ((r-neutralR)*intensity);
         pixels.data[i+1] = neutralG + ((g-neutralG)*intensity);
-        pixels.data[i+2] = pixels.data[i+2];
-        pixels.data[i+3] = pixels.data[i+3];
+        pixels.data[i+2] = neutralB - ((neutralB-b)*intensity);
+        pixels.data[i+3] = pixels.data[i+3]; //maintain the alpha channel
 
       }
 
